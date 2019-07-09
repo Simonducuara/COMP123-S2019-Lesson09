5106dedc2e5b13a52ffdc3b2c47be1f3ce429a6c
simon gonalez <scdg98@outlook.com>
2019-07-09 13:58:45 -0400

    Created base empty form

--- Building a Calculator/CalcuatorForm.cs
+++ Building a Calculator/CalcuatorForm.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace Building_a_Calculator
 {
-    public partial class Form1 : Form
+    public partial class CalcuatorForm : Form
     {
-        public Form1()
+        public CalcuatorForm()
         {
             InitializeComponent();
         }
--- Building a Calculator/Program.cs
+++ Building a Calculator/Program.cs
@@ -16,7 +16,7 @@ namespace Building_a_Calculator
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new CalcuatorForm());
         }
     }
 }
